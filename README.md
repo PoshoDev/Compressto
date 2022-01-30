620a6f85457d5e72311c25bd64a26a329b4f03d4
Rodrigo Gómez Maitret <green.tie.rgm@gmail.com>
2022-01-30 06:08:47 -0600

    Version 1.0

---
+++ README.md
@@ -0,0 +1,40 @@
+# Compressto 🎩
+Batch image compression, conversion and other tools; with continuous search options.
+
+## Usage
+
+Compressto works from the command line. Example as follows:
+
+```powershell
+compressto.exe --dir_from="source_folder" --dir_to="destination_folder" --quality=50 --format=jpg --str_remove="Hello" --str_put="GoodBye"
+```
+
+The above script will convert the images from "source_folder" and convert them into .jpg files in the "destination_folder" at 50% of quality while renaming parts of their filenames. Such as:
+
+| source_folder    | destination_folder |
+| ---------------- | ------------------ |
+| HelloWorld.png   | GoodByeWorld.jpg   |
+| DontSayHello.png | DontSayGoodBye.jpg |
+| IhateMyself.png  | IhateMyself.jpg    |
+
+
+
+## Options
+
+You can use multiple flags as parameters in order to work with Compressto. Here's a list of said flags:
+
+| Flag         | Type    | Default | Description                                                  |
+| ------------ | ------- | ------- | ------------------------------------------------------------ |
+| -h, --help   | -       | -       | Brings up the help message.                                  |
+| --dir_from   | String  | ""      | The directory where the original images are.                 |
+| --dir_to     | String  | ""      | The directory where you want to save your converted images.  |
+| --quality    | Int     | 90      | The quality of your converted image between **0-100**. Lower quality results in smaller file sizes. |
+| --format     | String  | "jpg"   | The extension of the resulting images. Currently tested with **'jpg'** and **'png'**. Files with 'jpg' extension tend to be smaller in size. |
+| --str_remove | String  | ""      | A substring you want to remove from the filename.            |
+| --str_put    | String  | ""      | A substring to replace **--str_remove** with.                |
+| --delete     | Boolean | False   | Whether you want to delete the original files once conversion is complete or not. |
+| --infinite   | Boolean | False   | Enables constant scanning on the source folder. If enabled, program must be terminated manually. |
+| --interval   | Int     | 60      | The time interval **(in seconds)** in which the program will scan for images if the **--infinite** flag is enabled. |
+| --hide       | Boolean | False   | Hides the program's console window. If the **--infinite** flag is enabled, then the program must be terminated from the Task Manager or other methods. |
+| --verbose    | Boolean | True    | If disabled, the program won't output any text.              |
+
--- compressto.py
+++ compressto.py
@@ -1,33 +1,63 @@
-import os, sys, time, glob, argparse
+import os, sys, time, glob, argparse, win32gui, win32con
 from PIL import Image
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dir_from", type=str, default="", help="lol")
-    parser.add_argument("--dir_to", type=str, default="", help="lol")
-    parser.add_argument("--quality", type=int, default=90, help="lol")
-    parser.add_argument("--format", type=str, default="jpg", help="lol")
-    parser.add_argument("--str_remove", type=str, default="", help="lol")
-    parser.add_argument("--str_put", type=str, default="", help="lol")
-    parser.add_argument("--delete", type=bool, default=False, help="lol")
-    parser.add_argument("--infinite", type=bool, default=False, help="lol")
-    parser.add_argument("--interval", type=int, default=60, help="lol")
+    parser.add_argument("--dir_from", type=str, default="",
+                        help="The directory where the original images are.")
+    parser.add_argument("--dir_to", type=str, default="",
+                        help="The directory where you want to save your "
+                        "converted images.")
+    parser.add_argument("--quality", type=int, default=90,
+                        help="The quality of your converted image between "
+                        "0-100. Lower quality results in smaller file sizes.")
+    parser.add_argument("--format", type=str, default="jpg",
+                        help="The extension of the resulting images. "
+                        "Currently tested with 'jpg' and 'png'. Files with "
+                        "'jpg' extension tend to be smaller in size.")
+    parser.add_argument("--str_remove", type=str, default="",
+                        help="A substring you want to remove from the "
+                        "filename.")
+    parser.add_argument("--str_put", type=str, default="",
+                        help="A substring to replace --str_remove with.")
+    parser.add_argument("--delete", type=bool, default=False,
+                        help="Whether you want to delete the original files "
+                        "once conversion is complete or not.")
+    parser.add_argument("--infinite", type=bool, default=False,
+                        help="Enables constant scanning on the source folder. "
+                        "If enabled, program must be terminated manually.")
+    parser.add_argument("--interval", type=int, default=60,
+                        help="The time interval (in seconds) in which the "
+                        "program will scan for images if the --infinite flag "
+                        "is enabled.")
+    parser.add_argument("--hide", type=bool, default=False,
+                        help="Hides the program's console window. If the "
+                        "--infinite flag is enabled, then the program must "
+                        "be terminated from the Task Manager or other methods.")
+    parser.add_argument("--verbose", type=bool, default=True,
+                        help="If disabled, the program won't output any text.")
     args = parser.parse_args()
-    print("C O M P R E S S T O !")
+    if args.hide:
+        hide = win32gui.GetForegroundWindow()
+        win32gui.ShowWindow(hide , win32con.SW_HIDE)
+    if args.verbose: print("  _   _       __  __  ___  __   __ ___ _\n"
+                           " / ` / ) )\/) )_) )_) )_  (_ ` (_ ` ) / )\n"
+                           "(_. (_/ (  ( /   / \ (__ .__) .__) ( (_/\n")
     compress_images(args)
 
 def compress_images(args):
     if args.dir_from != "":
         os.chdir(args.dir_from)
-    print(f"Looking for images in: {args.dir_from}...")
+    if args.verbose: print(f"Looking for images in: {args.dir_from}...")
     count = 0
     if args.infinite:
         while True:
             count += search_images(args)
             time.sleep(args.interval)
-            print("Searching again...")
+            if args.verbose: print("Searching again...")
     else: count += search_images(args)
-    print(f"Converted {count} images!" if count else "Nothing to convert!")
+    if args.verbose:
+        print(f"Converted {count} images!" if count else "Nothing to convert!")
 
 def search_images(args):
     files = os.listdir()
@@ -39,15 +69,16 @@ def search_images(args):
             image_final = image.replace(args.str_remove, args.str_put)[:-4]
             file_final = f"{args.dir_to}\\{image_final}.{args.format}"
             if not os.path.exists(file_final):
-                print(f"Saving: {image}")
+                if args.verbose: print(f"Saving: {image}")
                 img.save(file_final, optimize=True, quality=args.quality)
-                print(f"Saved: {image_final}.{args.format}!")
+                if args.verbose: print(f"Saved: {image_final}.{args.format}!")
                 if args.delete:
                     os.remove(image)
-                    print(f"Removed source: {image}")
+                    if args.verbose: print(f"Removed source: {image}")
                 count += 1
         except: pass
     return count
+
 
 if __name__ == "__main__":
     main()
