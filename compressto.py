a79b8c482395c15cf3536933c64a9e4bb8d72837
Rodrigo Gómez Maitret <green.tie.rgm@gmail.com>
2022-01-29 20:20:48 -0600

    Constant lookout.

--- compressto.py
+++ compressto.py
@@ -5,22 +5,26 @@ import glob
 def main(args):
     compress_images(*args)
 
-def compress_images(dir_from="", dir_to="", quality=90,
-                    format=".jpg", str_remove="", str_put=""):
+def compress_images(dir_from="", dir_to="", quality=90, format="jpg",
+                    str_remove="", str_put=""):
     if dir_from != "":
         os.chdir(dir_from)
-    files = os.listdir()
-    images = [file for file in files if file.endswith(("jpg", "png"))]
+    print(f"Looking for images in: {dir_from}")
     count = 0
-    for image in images:
-        img = Image.open(image)
-        image_final = image.replace(str_remove, str_put)[:-4]
-        file_final = f"{dir_to}\\{image_final}.{format}"
-        if not os.path.exists(file_final):
-            print(f"Saving: {image}")
-            img.save(file_final, optimize=True, quality=quality)
-            print(f"Saved: {image_final}.{format}!")
-            count += 1
+    while True:
+        files = os.listdir()
+        images = [file for file in files if file.endswith(("jpg", "png"))]
+        for image in images:
+            try:
+                img = Image.open(image)
+                image_final = image.replace(str_remove, str_put)[:-4]
+                file_final = f"{dir_to}\\{image_final}.{format}"
+                if not os.path.exists(file_final):
+                    print(f"Saving: {image}")
+                    img.save(file_final, optimize=True, quality=int(quality))
+                    print(f"Saved: {image_final}.{format}!")
+                    count += 1
+            except: pass
     print(f"Converted {count} images!" if count else "Nothing to convert!")
 
 if __name__ == "__main__":
